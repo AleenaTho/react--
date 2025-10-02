9633d10fccf122840141e6efa9dce8a50488b56d
Aleena-Thomas87 <aalleennaa2010@gmail.com>
2025-10-02 14:42:02 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// The variable name is correct.
+// The variable name is correct.firebase init hosting
+
 // NOTE: Your repository name is 'quote--' including the two dashes.
-const repoName = "quote--";
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
     // ❌ Error was here: bas: '/$repoName}/'
     // ✅ Fix: Use 'base' and the correct template literal syntax
-    base: `/${repoName}/`,
+    base: "/react--",
     plugins: [react()],
 })
